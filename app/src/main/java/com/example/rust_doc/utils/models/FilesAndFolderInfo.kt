fa5f50ad7e0cce2acde84c34938a934777536bad
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-08-03 23:19:14 +0600

    [fit] additional files added

--- app/src/main/java/com/example/rust_doc/utils/models/FilesAndFolderInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rust_doc.utils.models
-
-data class FilesAndFolderInfo(
-  var folderName: String,
-  var parent: String,
-  var files: MutableList<String> = mutableListOf<String>(),
-  var children: MutableList<FilesAndFolderInfo> = mutableListOf<FilesAndFolderInfo>(),
-
-  )
